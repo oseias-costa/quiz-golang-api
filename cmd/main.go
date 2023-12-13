2655c1368b584025d33c2a61abf8e2f51200ecd2
Oséias Costa <oseiasc2@gmail.com>
2023-12-13 20:47:01 -0300

    feat(go mod): create go mod init

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gorilla/mux"
+	"github.com/oseias-costa/quiz-golang-api/handler"
+)
+
+func main() {
+	r := mux.NewRouter()
+	port := ":8000"
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hello"))
+	})
+	r.HandleFunc("/posts", handler.GetAllPosts).Methods(http.MethodGet)
+
+	http.ListenAndServe(port, r)
 }
