bc3ee2984220a3114f96b8b47e12753242035342
Oséias Costa <oseiasc2@gmail.com>
2023-12-17 10:47:14 -0300

    fix(main.go): initialized client to firebase

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
+	"context"
 
 	"github.com/gorilla/mux"
 	"github.com/oseias-costa/quiz-golang-api/handler"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
 func main() {
 	r := mux.NewRouter()
 	port := ":8000"
+
+	opt := option.WithCredentialsFile("/home/oseias-costa/test-cfcbf-firebase-adminsdk-9keyk-2ae765abd7.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
